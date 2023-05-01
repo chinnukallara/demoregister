80c7b9ae93056388a757f41910ae6037522993fe
chinnukallara <chinnukallara11@gmail.com>
2023-05-01 12:50:31 +0530

    project

--- features/demoregister.feature
+++ features/demoregister.feature
@@ -1,7 +1,12 @@
 Feature: The Automation Testing Website demo
 
-  Scenario Outline: As a user, I register the details
+  Scenario Outline: As a user, Performing Login
 
-    Given I am on the Register page
-    When I Enter the details
+    Given I am on the login page
+    When I enter the <username> and <password>
     Then I enter a valid page
+
+    Examples:
+        |username|password|
+        |chinnu|chinnu123|
+        
--- features/step-definitions/demoregisterstep.js
+++ features/step-definitions/demoregisterstep.js
@@ -1,14 +1,14 @@
 import { Given,When,Then } from "@wdio/cucumber-framework";
 import demoregisterpage from "../pageobjects/demoregisterpage";
-Given(/^I am on the Register page$/,async()=>{
+Given(/^I am on the login page$/,async()=>{
 
 await demoregisterpage.openbrowser();
 })
-When(/^I Enter the details$/,async()=>{
-    await demoregisterpage.login();
+When(/^I enter the (\w+) and (.+)$/,async(username,password)=>{
+    await demoregisterpage.login(username,password);
     
 })
 Then(/^I enter a valid page$/,async()=>{
     await demoregisterpage.message();
-    
+  //  await demoregisterpage.openbrowser();
 })
